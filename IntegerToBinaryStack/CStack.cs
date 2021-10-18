48e1670cd3a6cc27504f0559c3d899840420b0bf
Rayhaan <rayhaannak1@gmail.com>
2021-10-18 11:56:02 +0200

    first commit

---
+++ IntegerToBinaryStack/CStack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegerToBinaryStack
+{
+   public class CStack
+    {
+
+		public void intToBin(int number)
+		{
+			Stack<int> stk = new Stack<int>();
+			while (number > 0)
+			{
+				int rem = number % 2; //take remainder
+				number = number / 2;
+				stk.Push(rem);
+			}
+			while (stk.Count > 0)
+			{
+				int item;
+				item = stk.Peek();
+				stk.Pop();
+				Console.Write(item);
+			}
+		}
+
+	}
+}
---
+++ IntegerToBinaryStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IntegerToBinaryStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CStack stk = new CStack();
+            string number;
+
+            Console.Write("Enter the integer : ");
+
+            number = Console.ReadLine();
+
+            int num = Convert.ToInt32(number);
+     
+            stk.intToBin(num);
+
+        }
+
+
+
+
+    }
+}
